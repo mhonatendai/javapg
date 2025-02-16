a01bcf63c137d1280d4eb985e25a69176b7fa973
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-16 16:49:36 -0500

    more List methods

--- src/main/java/org/javapg/collections/ListAndSet.java
+++ src/main/java/org/javapg/collections/ListAndSet.java
@@ -1,8 +1,6 @@
 package org.javapg.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 public class ListAndSet {
     public static void main(String[] args) {
@@ -10,12 +8,16 @@ public class ListAndSet {
 
         System.out.println("LIST");
         Collection<String> list = new ArrayList<>();
-        System.out.println(list.add("Sparrow")); // prints true, add () returns a boolean of whether the addition was successful.
+
+//add () returns a boolean of whether the addition was successful.
+        System.out.println(list.add("Sparrow")); // prints true,
         System.out.println(list.add("Sparrow")); // prints true as well.
         System.out.println(list.add("Another")); // prints true as well.
         System.out.println();
         System.out.println(list);
-        list.removeIf(s -> s.startsWith("A"));// Uses a predicate to remove with a condition.
+
+//Uses a Predicate to remove with a condition.
+        list.removeIf(s -> s.startsWith("A"));
         System.out.println(list);
 
         System.out.println();
@@ -33,6 +35,44 @@ public class ListAndSet {
         System.out.println(set.isEmpty());  //false, because the set is not empty.
         System.out.println(set.size());     // number of elements in the set.
 
+        set.add("");
+        set.removeIf(String::isEmpty); // s -> s.isEmpty()
+        System.out.println(set);
+
         set.clear();  // empties the set.
+
+        String[] arrayOriginal = new String[] {"a", "b", "c"};
+        List<String> asList = Arrays.asList(arrayOriginal); // [a, b, c]
+        List<String> fromOf = List.of(arrayOriginal); // [a, b, c]
+        List<String> copyCreation = List.copyOf(asList); // [a, b, c]
+
+        arrayOriginal[0] = "za";
+        System.out.println(fromOf);
+        System.out.println(copyCreation);
+        System.out.println(asList);
+        System.out.println();
+//        fromOf.set(0,"t");  UnSupportedOperationException
+
+//        What do you think?
+        System.out.println("Integer List after removing integers and removing at index ...");
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(3);
+        integerList.add(2);
+        integerList.add(1);
+        integerList.remove(2); //Removes element at index 2.
+        integerList.remove(Integer.valueOf(2)); //Removes the value 2, takes in the Integer object.
+
+        System.out.println(integerList);
+
+        List<String> listNewOne = new ArrayList<>();
+        listNewOne.add("hawk");
+        listNewOne.add("robin");
+        Object[] objectArray = listNewOne.toArray();
+        String[] stringArray = listNewOne.toArray(new String[0]);
+        System.out.println("Elements of stringArray");
+        System.out.println(Arrays.asList(stringArray));
+        listNewOne.clear();
+        System.out.println(objectArray.length); // 2
+        System.out.println(stringArray.length); // prints 2 as well, arrays do not grow / shrink.
     }
 }
