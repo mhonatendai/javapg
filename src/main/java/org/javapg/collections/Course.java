cde31d3a4634ba60bcbde3030660e4a50f0844f9
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-18 18:09:39 -0500

    Removing assumptions and adding exceptions.

--- src/main/java/org/javapg/collections/Course.java
+++ src/main/java/org/javapg/collections/Course.java
@@ -13,6 +13,13 @@ public class Course implements Comparable<Course>{
     }
 
     public int compareTo(Course course) {
+        if (course == null)
+            throw new IllegalArgumentException("No course!");
+        if (this.name == null && course.name == null)
+            return 0;
+        else if (this.name == null) return -1;
+        else if (course.name == null) return 1;
+        else
         return name.compareTo(course.name); // sorts by name in ascending order.
     }
 }
