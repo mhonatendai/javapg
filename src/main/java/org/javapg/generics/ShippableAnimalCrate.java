22e9f8903f928b03757171cab85b155369b351bf
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-19 15:13:32 -0500

    Unbounded wildcard in generics.

--- src/main/java/org/javapg/generics/ShippableAnimalCrate.java
+++ src/main/java/org/javapg/generics/ShippableAnimalCrate.java
@@ -1,5 +1,17 @@
 package org.javapg.generics;
 
+import java.util.List;
+
 class ShippableAnimalCrate implements Shippable<Animal> {
-public void ship(Animal t) { }
+    public void ship(Animal t) {
+    }
+/*
+An example of an unbounded wildcard in generics.
+ */
+    public static void shipAnything(List<Object> list) {
+        for (Object x: list)
+            System.out.println(x);
+    }
+
+
 }
