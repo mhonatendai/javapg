0546063901585e9ba3cfdf24777ddd1f7e0c27dc
Tendai E. Mhona <mhonatendai@gmail.com>
2025-02-19 15:23:23 -0500

    Upper bounded wildcard for generics

--- src/main/java/org/javapg/generics/ShippableAnimalCrate.java
+++ src/main/java/org/javapg/generics/ShippableAnimalCrate.java
@@ -1,5 +1,6 @@
 package org.javapg.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class ShippableAnimalCrate implements Shippable<Animal> {
@@ -11,6 +12,13 @@ An example of an unbounded wildcard in generics.
     public static void shipAnything(List<?> list) {
         for (Object x: list)
             System.out.println(x);
+
+//        The line below does not compile but an upper-bounded wildcard can solve the issue.
+//        ArrayList<Number> liste = new ArrayList<Integer>();
+
+
+//        It says any class that extends Number or Number itself can be used as the formal parameter type
+        List<? extends Number> listNumbers = new ArrayList<Integer>();
     }
 
 
